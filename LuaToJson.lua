2c2cba8d4747a80273f44259164e957ff84dc2e2
Mark.Ma <markma08181212@gmail.com>
2024-10-02 20:44:21 -0400

    Gen mod data in json format

---
+++ LuaToJson.lua
@@ -0,0 +1,23 @@
+local json = require("dkjson")
+local modItem = require("ModItem")
+
+local sortedData = {}
+for key, value in pairs(modItem) do
+    local itemType = value.type
+    if not sortedData[itemType] then
+        sortedData[itemType] = {}
+    end
+    table.insert(sortedData[itemType], value)
+end
+
+for itemType, items in pairs(sortedData) do
+    local filePath = string.format("%s.json", itemType)
+    local content = json.encode(items, { indent = true })
+    local file = io.open(filePath, "w")
+    if not file then return nil end
+    file:write(content)
+    file:close()
+end
+
+
+
